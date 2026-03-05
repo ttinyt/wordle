74bae6dd7f72a1d614cc7e076467b5e56deb97ae
tt <tinyin@proton.me>
2026-03-05 23:55:31 +0100

    lesson 9

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -6,8 +6,8 @@ import Row from './Row'
 export default function Grid({ guesses, currentGuess, turn }) {
     return (
         <div>
-            {guesses.map((g, i) => {
-                return <Row key={i} />
+            {guesses.map((g, i) => { //g is going to be a past formatted guess
+                return <Row key={i} guess={g} />
             })}
         </div>
     )
