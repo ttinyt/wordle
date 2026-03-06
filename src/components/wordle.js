b0de8ac4e39436595aea9e10aed17b2fbdc725ef
tt <tinyin@proton.me>
2026-03-06 13:40:56 +0100

    adding logic for modal when word is correct or out of turns

--- src/components/wordle.js
+++ src/components/wordle.js
@@ -1,23 +1,24 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import useWordle from '../hooks/useWordle'
 //components
 import Grid from './Grid'
 import Keypad from './Keypad'
+import Modal from './Modal'
 
 export default function Wordle({ solution }) {
     const { currentGuess, handleKeyup, guesses, isCorrect, turn, usedKeys } = useWordle(solution)
-
+    const [showModal, setShowModal] = useState(false)
 
     useEffect(() => {
         window.addEventListener('keyup', handleKeyup)
 
         if(isCorrect){
-            console.log('congrats you win!!')
+            setTimeout(() => setShowModal(true), 2000)
             window.removeEventListener('keyup', handleKeyup)
         }
 
         if (turn > 5){
-            console.log('out of guesses booo')
+            setTimeout(() => setShowModal(true), 2000)
             window.removeEventListener('keyup', handleKeyup)
         }
 
@@ -31,6 +32,7 @@ export default function Wordle({ solution }) {
             <div>Current Guess - {currentGuess}</div>
             <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
             <Keypad usedKeys={usedKeys} />
+            {showModal && <Modal isCorrect={isCorrect} turn={turn} solution={solution} />}
         </div>
     )
 }
