221dbeeb0cb11bc9fc4bf24d09fb5f7755cbbd09
tt <tinyin@proton.me>
2026-03-06 15:04:46 +0100

    backspace icon fix. and onclick handler for handleclick, so that we kan click on the keypad as well.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,6 +1,6 @@
-import backspace from './assets/backspace-icon.jpg'; 
+import backspace from '../assets/backspace-icon.png';
 
-export default function Keypad({ usedKeys }) {
+export default function Keypad({ usedKeys, handleClick }) {
     const topRow = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"]
     const middleRow = ["a", "s", "d", "f", "g", "h", "j", "k", "l"]
     const bottomRow = ["z", "x", "c", "v", "b", "n", "m"]
@@ -10,37 +10,41 @@ export default function Keypad({ usedKeys }) {
             {topRow.map((l) => {
                 const color = usedKeys[l]
                 return (
-                    <div key={l} className={color}>{l}</div>
+                    <div key={l} className={color} onClick={() => handleClick(l)}>{l}</div>
                 )
             })}
             <br />
+
             {middleRow.map((l) => {
                 const color = usedKeys[l]
                 return (
-                    <div key={l} className={color}>{l}</div>
+                    <div key={l} className={color} onClick={() => handleClick(l)}>{l}</div>
                 )
             })}
             <br />
-            <div key="Enter" style={{ width: "15%", fontSize: "13px" }}>Enter</div>
+
+            <div key="Enter" style={{ width: "15%", fontSize: "12px" }} onClick={() => handleClick('Enter')}>
+                Enter
+            </div>
             {bottomRow.map((l) => {
                 const color = usedKeys[l]
                 return (
-                    <div key={l} className={color}>{l}</div>
+                    <div key={l} className={color} onClick={() => handleClick(l)}>{l}</div>
                 )
             })}
             <div key="Backspace" style={{
                 width: "15%", textAlign: "center", whiteSpace: "nowrap"
             }}
-                onClick={() => handleKeyupVirtual("Backspace")}>
+                onClick={() => handleClick("Backspace")}>
 
                 <span className="helper" style={{
                     display: "inline-block", height: "100%", verticalAlign: "middle"
                 }}>
                 </span>
-                <img src={backspace} alt = "backspace-icon"
+                <img src={backspace} alt="backspace-icon"
                     style={{ width: "20px", verticalAlign: "middle" }}>
                 </img>
-            </div>        
+            </div>
         </div>
     )
 }
